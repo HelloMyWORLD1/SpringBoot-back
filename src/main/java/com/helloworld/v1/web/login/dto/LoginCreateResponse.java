5b2be921e268ad1e5e55de333c223d95f17ebf43
yongjae5717 <dydwo5717@naver.com>
2022-10-30 15:55:07 +0900

    login response수정

--- src/main/java/com/helloworld/v1/web/login/dto/LoginCreateResponse.java
+++ src/main/java/com/helloworld/v1/web/login/dto/LoginCreateResponse.java
@@ -1,16 +1,2 @@
-package com.helloworld.v1.web.login.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class LoginCreateResponse {
-    private int status;
-    private String message;
-    private String errors;
-    private String token;
-    private UserDto user;
+package com.helloworld.v1.web.login.dto;public class LoginCreateResponse {
 }
